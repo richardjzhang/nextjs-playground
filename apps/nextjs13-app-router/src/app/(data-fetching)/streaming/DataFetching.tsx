ad6680bf1b749b03658a624e4f749c0c6ee67cc0
richardjzhang <hello@richardjzhang.com>
2024-03-28 10:45:15 +1100

    Adjust delay time

--- apps/nextjs13-app-router/src/app/(data-fetching)/streaming/DataFetching.tsx
+++ apps/nextjs13-app-router/src/app/(data-fetching)/streaming/DataFetching.tsx
@@ -2,7 +2,17 @@
 import { Card } from "ui";
 
 export default async function DataFetching() {
-  await new Promise((resolve) => setTimeout(resolve, 5000));
+  const delayTime = 5000;
+  const dummyFetch = await fetch(
+    // We intentionally delay the response to simulate a slow data
+    // request that would benefit from streaming
+    `https://app-router-api.vercel.app/api/products?delay=${delayTime}&filter=1`,
+    {
+      // We intentionally disable Next.js Cache to better demo
+      // streaming
+      cache: "no-store",
+    }
+  ).then((res) => res.json());
   return (
     <Card>
       <div className="h-full flex justify-center items-center text-xl text-white">
